ae14366f0cfb4dbae0388c6dab2db3c25b006f1a
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-02-22 20:13:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,15 +9,27 @@ function setup() {
     tint_color="";
 }
 
+setInterval(function(){
+    x=Math.floor(Math.random()*5);
+    if(x==1){
+tint("red");
+    }
+if(x==2){
+tint("blue");
+    }
+if(x==3){
+tint("yellow");
+    }
+if(x==4){
+    tint("green");
+}
+},50);
+
 function draw() {
     image(video,0,0,640,480);
-    tint(tint_color);
+    
 }
 
 function take_snapshot(){
     save('tinted_background.png');
-}
-
-function filter_tint() {
-    tint_color=document.getElementById("color_name").value;
 }
